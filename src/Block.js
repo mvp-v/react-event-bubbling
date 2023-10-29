a40a564f1a8384c116e115a4c57bf8f338496103
Mikhail Petrov <for.mvpetrov@gmail.com>
2023-10-29 22:19:16 +0100

    color stop propagation log

--- src/Block.js
+++ src/Block.js
@@ -1,11 +1,14 @@
 import { forwardRef } from "react";
 
-export const Block = forwardRef(({children, id, stopPropagation}, ref) => (
+export const Block = forwardRef(({ children, id, stopPropagation }, ref) => (
   <div
     ref={ref}
     className={id}
     onClick={(e) => {
-      console.log(`%cReact onClick ${id}`, 'color: blue', stopPropagation ? 'stop propagation' : '');
+      console.log(
+        `%cReact onClick ${id} ${stopPropagation ? "stop propagation" : ""}`,
+        "color: blue"
+      );
       if (stopPropagation) {
         e.stopPropagation();
       }
